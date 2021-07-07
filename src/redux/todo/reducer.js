f9df5d8ef1b94bd68f3362980a674111c883aecb
pooulad <poooooladi@gmail.com>
2021-07-07 22:13:13 +0430

    reducer added

--- src/redux/todo/reducer.js
+++ src/redux/todo/reducer.js
@@ -9,7 +9,33 @@ const initialState = {
 const reducer = (state = initialState,action) => {
     switch(action.type){
         case ADD_TODO : 
+            return{
+                ...state,
+                data : [
+                    ...state.data,
+                    {
+                        id : action.payload.id,
+                        content : action.payload.content,
+                        complete : false
+                    }
+                ]
+            }
         case TOGGLE_TODO : 
+            const { id } = action.payload;
+            return{
+                ...state,
+                data : state.data.map((row) => {
+                    if (row.id === id) {
+                        return {
+                            id : row.id,
+                            content : row.content,
+                            complete : !row.complete,
+                        }
+                    }else{
+                        return row
+                    }
+                })
+            }
         default : return state
     }
 }
