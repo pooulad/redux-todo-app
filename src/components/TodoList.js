c5d03545e9c0ad250a21ea7cf0d4b41f445229b4
pooulad <poooooladi@gmail.com>
2021-07-07 22:13:37 +0430

    TodoList added

---
+++ src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from "react-redux";
+import Todo from "./Todo";
+
+
+function TodoList(props) {
+    return (
+        <div>
+            <div>
+                {
+                    props.todos && props.todos.length ?
+                        props.todos.map((row, index) => {
+                            return <Todo key={index} row={row} />
+                        }) : <div>nothing to doing</div>
+                }
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        todos: state.data
+    }
+}
+
+export default connect(mapStateToProps)(TodoList)
