818e6d1a18b8e1364481de544fefd0e98dc6ac88
pooulad <poooooladi@gmail.com>
2021-07-07 22:50:30 +0430

    AddTodo.js updated

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { addTodo } from "../redux/todo/actions"
+import "./AddTodo.css";
 
 class AddTodo extends Component {
     constructor(props){
         super(props);
         this.state = {
             input : "",
+            error : ""
         }
     }
     changeHandler = (event) => {
         this.setState({input : event.target.value})
     }
     clickHandler = () => {
-        this.props.addTodo(this.state.input)
-        this.setState({input : ""})
+        if (this.state.input !== "") {
+            this.props.addTodo(this.state.input)
+            this.setState({input : ""})
+        }
     }
     render() {
         return (
-            <div>
-                <input type="text" value={this.state.input} onChange={this.changeHandler} placeholder="Write Your Todo" />
-                <button onClick={this.clickHandler}>Add</button>
+            <div className="addTodo">
+                <input className="addInput" type="text" value={this.state.input} onChange={this.changeHandler} placeholder="Write Your Todo" />
+                <button className="addBtn" onClick={this.clickHandler}>Add</button>
             </div>
         )
     }
